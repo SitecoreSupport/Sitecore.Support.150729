67b877a96873e506b94571d143a7713d28e0673d
sc-spl <spl@sitecore.net>
2017-03-06 13:56:09 +0200

    Initial commit

---
+++ src/Sitecore.Support.150729/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.150729")]
+[assembly: AssemblyProduct("Sitecore.Support.150729")]
+[assembly: ComVisible(false)]
